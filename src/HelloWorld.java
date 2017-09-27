c5ee3afe68ef17c73bac9f0e7a73f1121e8d188a
toma <tomachi514@softbank.ne.jp>
2017-09-27 11:46:00 +0900

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+
+/**
+ * @author internousdev
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("HelloWorld");
+
+	}
+
+}
